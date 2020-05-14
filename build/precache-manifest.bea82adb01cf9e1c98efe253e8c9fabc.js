10c0f04abe60aaae5f8029e7c13f14a4beb14f91
lliqi <614124284@qq.com>
2020-05-14 15:18:41 +0800

    add

---
+++ build/precache-manifest.bea82adb01cf9e1c98efe253e8c9fabc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241af96d621c36d1f780c8c29f073415",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b1ef74ecf699b328b24",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d34294adfdd40e0eb1b8",
+    "url": "./static/js/2.45cafd27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.45cafd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1ef74ecf699b328b24",
+    "url": "./static/js/main.e6d36f39.chunk.js"
+  },
+  {
+    "revision": "2e8dc7d6c2c28ecd6173",
+    "url": "./static/js/runtime-main.770f0b39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
